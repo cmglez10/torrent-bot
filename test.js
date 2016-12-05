24690ff8886a45a4b42e6e7978f55747484fcf97
leader <cmglez10@gmail.com>
2016-12-05 11:22:06 +0100

    adding a torrent working. Fuck yeah!

--- test.js
+++ test.js
@@ -9,38 +9,48 @@ var gateway = require('./lib/gateways/' + config.gateway.type)(config.gateway)
 var seedbox = require('./lib/seedboxs/' + config.seedbox.type)(config.seedbox, gateway)
 var tracker = require('./lib/trackers/' + config.tracker.type)(config.tracker, gateway)
 
-let options = {
+var options = {
    method: 'GET',
    uri: "https://hdcity.li/download.php?id=4c86c1d6f044206da533f9d99d1ac270cc734578&f=The%20five%201x02%20BDRip%201080p%20%5BDual%2BSub%5D%5BHDTEAM%5D.torrent&auth=MjczYmJmYTdlZi05MjQ5MjQtNTZkMTA0NjVhNC00MjAyNDQ=",
    resolveWithFullResponse: true,
-   encoding: 'ascii'
+   encoding: 'base64'
 }
 
 rp(options)
    .then((page)=> {
 
-      let body = Buffer.from(page.body)
-      let base64 = body.toString('base64')
-
-      seedbox.addTorrent(base64)
+      var body = Buffer.from(page.body)
+       fs.writeFile('cloud.torrent', body, function (err) {
+           if (err) return console.log(err);
+           // console.log('Hello World > helloworld.txt');
+       });
+       // fs.writeFile('headers.txt', JSON.stringify(page.headers, null, 2), function (err) {
+       //     if (err) return console.log(err);
+       //     // console.log('Hello World > helloworld.txt');
+       // });
+       // var base64 = Buffer.from(page.body).toString('base64')
+       // // console.log(base64)
+       // fs.writeFile('base64online.txt', base64, function (err) {
+       //     if (err) return console.log(err);
+       // });
+      // seedbox.addTorrent(base64)
 
    })
    .catch((err) => {
       console.log(err)
    })
 
-//
-// fs.readFile("",'ascii',function(err,data) {
-//    fs.writeFile('file.txt', data, function (err) {
-//       if (err) return console.log(err);
-//       // console.log('Hello World > helloworld.txt');
-//    });
-//    let base64 = Buffer.from(data).toString('base64')
-//    // console.log(base64)
-//    // fs.writeFile('base64.txt', base64, function (err) {
-//    //    if (err) return console.log(err);
-//    //    // console.log('Hello World > helloworld.txt');
-//    // });
-//    seedbox.addTorrent(base64)
-// });
+
+fs.readFile("file.torrent",'base64',function(err,data) {
+   fs.writeFile('filetor.torrent', data, function (err) {
+      if (err) return console.log(err);
+      // console.log('Hello World > helloworld.txt');
+   });
+   //  var base64 = Buffer.from(data).toString('base64')
+   // // console.log(base64)
+   //  fs.writeFile('base64.txt', base64, function (err) {
+   //      if (err) return console.log(err);
+   //  });
+   // seedbox.addTorrent(base64)
+});
 
