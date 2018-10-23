bb9e85a43fb2b25b8008d0da6b6ded189e3e760f
César González <cgonzalez@buguroo.com>
2018-10-23 12:20:08 +0200

    Added +HacheDe tracker and support to various trackers configuration

--- config-sample.js
+++ config-sample.js
@@ -7,37 +7,40 @@ var types = require('./lib/types')
 var qualities = require('./lib/qualities')
 
 module.exports =
-{
-   database: {
-      uri: '<URL>:<PORT>/<DATABASE>',
-      user: '<USER>',
-      password: '<PASSWORD>'
-   },
-   refreshInterval: 60 * 60 * 1000, // En milisegundos
-   seedbox: {
-      type: 'transmission',
-      user: "<transmission user>",
-      pass: "<transmission pass>",
-      rpc: "http://<transmission host>:<transmission port>/transmission/rpc"
-   },
-   tracker: {
-      type: 'hdcity',
-      url: "<tracker rss url>"
-   },
-   gateway: {
-      type: 'telegram',
-      token: '<telegram token>',
-      userId: 0 // <telegram userId>
-   },
-    // TViso API is not working
-   /*info: {
-      type: 'tviso',
-      idapi: '<tviso idAPI>',
-      secret: '<tviso secret>'
-   }*/
-   info: {
-      type: 'trakt-tv',
-      clientId: '5d019829b09612b23b4dbe5ab5026f4d81f37638363637541d7fd3bc64b750e0',
-      clientSecret: '2d938ed974d1fe32c9b80fe6a1b34969062c7d8891a3d3f6f3be658b1eb190cd'
-   }
-}
+	{
+		database: {
+			uri: '<URL>:<PORT>/<DATABASE>',
+			user: '<USER>',
+			password: '<PASSWORD>'
+		},
+		refreshInterval: 60 * 60 * 1000, // En milisegundos
+		seedbox: {
+			type: 'transmission',
+			user: "<transmission user>",
+			pass: "<transmission pass>",
+			rpc: "http://<transmission host>:<transmission port>/transmission/rpc"
+		},
+		trackers: [{
+			name: 'hdcity',
+			url: '<tracker rss url>'
+		}, {
+			name: 'hachede',
+			url: '<tracker rss url>'
+		}],
+		gateway: {
+			type: 'telegram',
+			token: '<telegram token>',
+			userId: 0 // <telegram userId>
+		},
+		// TViso API is not working
+		/*info: {
+			 type: 'tviso',
+			 idapi: '<tviso idAPI>',
+			 secret: '<tviso secret>'
+		}*/
+		info: {
+			type: 'trakt-tv',
+			clientId: '<trakt-tv clientId>',
+			clientSecret: '<trakt-tv clientSecret>'
+		}
+	}
--- index.js
+++ index.js
@@ -3,13 +3,20 @@ var Promise = require('bluebird')
 global.Promise = Promise
 
 Promise.config({
-    cancellation: true
-  });
-  
+  cancellation: true
+});
+
 var gateway = require('./lib/gateways/' + config.gateway.type)(config.gateway)
 var seedbox = require('./lib/seedboxs/' + config.seedbox.type)(config.seedbox, gateway)
-var tracker = require('./lib/trackers/' + config.tracker.type)(config.tracker, gateway)
-var info    = require('./lib/info/' + config.info.type)(config.info)
+var info = require('./lib/info/' + config.info.type)(config.info)
+var trackers = [];
+config.trackers.forEach((trackerConfig) => {
+  const trackerCode = require('./lib/trackers/' + trackerConfig.name)(trackerConfig, gateway);
+  trackers.push({
+    name: trackerConfig.name,
+    code: trackerCode
+  });
+});
 
 var filterUtils = require('./lib/filters/utils')
 const filters = filterUtils.parseFilters(config.filters)
@@ -23,23 +30,24 @@ global.types = types
 // Movidas de mongoose
 var mongoose = require('mongoose');
 global.mongoose = mongoose;
+mongoose.set('debug', true);
 
-mongoose.connect('mongodb://'+config.database.user+':'+config.database.password+'@'+config.database.uri, { useNewUrlParser: true })
-    .then((db) => {
-        console.log("Connection established with MongoDB");
-    })
-    .catch (() => {
-        console.error.bind(console, 'connection error:')
-    });
+mongoose.connect('mongodb://' + config.database.user + ':' + config.database.password + '@' + config.database.uri, { useNewUrlParser: true })
+  .then((db) => {
+    console.log("Connection established with MongoDB");
+  })
+  .catch(() => {
+    console.error.bind(console, 'connection error:')
+  });
 
 //-- Schemas
 var filterSchema = mongoose.Schema({
-   name: String,
-   type: String,
-   tracker: String,
-   path: String,
-   image: String,
-   terms: [String]
+  name: String,
+  type: String,
+  tracker: String,
+  path: String,
+  image: String,
+  terms: [String]
 });
 
 var Filter = mongoose.model('Filter', filterSchema, 'Filter');
@@ -54,15 +62,17 @@ setInterval(sendNewTorrents, config.refreshInterval)
 
 var lastUpdate;
 function sendNewTorrents() {
-   console.log(new Date().toISOString()+" - BUSCANDO NUEVOS TORRENTS")
-   tracker
+  console.log(new Date().toISOString() + " - BUSCANDO NUEVOS TORRENTS")
+  trackers.forEach((tracker) => {
+    tracker.code
       .latest()
       .then(result => {
-         //filtro para quedarme sólo con los posteriores a la fecha de ultima actualización
-         result = _.filter(result, i => !(lastUpdate && i.date < lastUpdate))
+        //filtro para quedarme sólo con los posteriores a la fecha de ultima actualización
+        result = _.filter(result, i => !(lastUpdate && i.date < lastUpdate))
 
-         //Buscamos los filtros de hdcity para pasarlos a la función que aplica filtros
-         Filter.find({tracker: "hdcity"}, function (err, filters){
+        //Buscamos los filtros de hdcity para pasarlos a la función que aplica filtros
+        Filter.find().or([{ tracker: tracker.name }, { tracker: '*' }])
+          .then(filters => {
             // console.log(JSON.stringify(filters))
             result = applyFilters(result, filters)
             // console.log("resultado filtrado:")
@@ -70,58 +80,59 @@ function sendNewTorrents() {
             result = _.sortBy(result, ['date'])
 
             Promise
-               .all(_.map(result, o => info.fill(o)))
-               .then(()=>{
-                  Promise.each(result, function(info){
-                     console.log(info.title)
-                     return gateway.sendTorrentInfo(info)
-                  })
-               })
+              .all(_.map(result, o => info.fill(o)))
+              .then(() => {
+                Promise.each(result, function (info) {
+                  console.log(info.title)
+                  return gateway.sendTorrentInfo(info)
+                })
+              })
             lastUpdate = new Date()
-         })
+          })
       })
+  })
 }
 
 /*
    Devuelve sólo aquellos registros de infoArray que pasan algún filtro de filters
  */
 function applyFilters(infoArray, filters) {
-    return _.filter(infoArray, function (info) {
-        var title = cleanTermToSearch(info.titleAll)
-        var res = false
+  return _.filter(infoArray, function (info) {
+    var title = cleanTermToSearch(info.titleAll)
+    var res = false
 
-        //Miramos a ver si coincide con alguno de los filtros
-        filters.forEach((filter) => {
-            var partialres = true
+    //Miramos a ver si coincide con alguno de los filtros
+    filters.forEach((filter) => {
+      var partialres = true
 
-            //Debe coincidir con todos los términos del filtro
-            filter.terms.forEach((term) => {
-                var clenanedterm = cleanTermToSearch(term)
-                if (!_.includes(title, clenanedterm)) partialres = false
-            })
-            if (partialres){
-               res = true
-               info.titleSearch = filter.name;
-               info.path = filter.path;
-               info.image = filter.image;
-            }
-        })
-        return res
+      //Debe coincidir con todos los términos del filtro
+      filter.terms.forEach((term) => {
+        var clenanedterm = cleanTermToSearch(term)
+        if (!_.includes(title, clenanedterm)) partialres = false
+      })
+      if (partialres) {
+        res = true
+        info.titleSearch = filter.name;
+        info.path = filter.path;
+        info.image = filter.image;
+      }
     })
+    return res
+  })
 }
 
-function cleanTermToSearch(term){
-   var unescapeterm = unescape(term)
-    return unescapeterm.toLowerCase()
+function cleanTermToSearch(term) {
+  var unescapeterm = unescape(term)
+  return unescapeterm.toLowerCase()
 }
 
 
 
 gateway.onRequestAddTorrent(function (msg, path) {
-   tracker.decodeTorrent(msg)
-      .then((torrent) => {
-         console.log("Added: " + msg)
-         return seedbox.addTorrent(torrent, path)
-      })
-   }
+  tracker.decodeTorrent(msg)
+    .then((torrent) => {
+      console.log("Added: " + msg)
+      return seedbox.addTorrent(torrent, path)
+    })
+}
 )
